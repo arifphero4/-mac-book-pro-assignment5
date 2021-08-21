76ed5bbc17f0589001ee4b68b0ec2983b083b059
arifphero <ahammadarif232@gmail.com>
2021-08-21 12:13:29 +0600

    first commit

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+/* buttons assignment */
+const button8Gbmemory = document.getElementById('8-gb-memory');
+const button16Gbmemory = document.getElementById('16-gb-memory');
+const button256Gbtorage = document.getElementById('256-gb-storage');
+const button512GbStorage = document.getElementById('512-gb-storage');
+const button1TbStorage = document.getElementById('1-tb-storage');
+const buttonDeliveryBy25 = document.getElementById('delivery-by-25');
+const buttonDeliveryBy21 = document.getElementById('delivery-by-21');
+const buttonApplyPromo = document.getElementById('promo-code-button');
+
+/* variables assignment */
+const bestPrice = document.getElementById('best-price');
+const extraMemoryCost = document.getElementById('memory-cost')
+const extraStorageCost = document.getElementById('storae-cost');
+const deliveryCharge =  document.getElementById('delivery-charge');
+const totalPrice = document.getElementById('total-cost');
+const finalTotal = document.getElementById('final-total');
+const promoCodeInput = document.getElementById('promo-code-input');
+
